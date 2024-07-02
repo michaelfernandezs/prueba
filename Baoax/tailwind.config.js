185b17178ae8140b2871cccdf4cddd196ef684f7
MICHAL FERNANDEZ <michaeltkd14@gmail.com>
2024-07-02 15:27:55 -0600

    Signed-off-by: MICHAL FERNANDEZ <michaeltkd14@gmail.com>

--- Baoax/tailwind.config.js
+++ Baoax/tailwind.config.js
@@ -1,4 +1,5 @@
 
+const colors=require('tailwindcss/colors')
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [    "./index.html",
@@ -7,6 +8,31 @@ export default {
 
 
   theme: {
+    colors: {
+     blue:{
+      500:'#C4C6FD',
+      600:'#777BF9',
+      750:'#6267F8',
+      800:'#5055F7',
+      850:'#3C42F6',
+      900:'#2930F5'
+     },
+     fuchsia:{
+      500:'#24014B',
+      600:'#43058A',
+      800:'#300462',
+      
+     },
+     slate:{
+      800:'#060DDB',
+      900:'#060049'
+      },
+      white:{
+        500:'#FFFFFF'
+        }
+      
+    },
+
     extend: {},
   },
   plugins: [],
